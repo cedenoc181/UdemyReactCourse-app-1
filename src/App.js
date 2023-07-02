e51f6a67b697f92815c010765ba96957bb6b0eab
Christian <cedenoc181@gmail.com>
2023-07-02 19:31:14 -0400

    added a test movie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 import {useEffect} from "react";
+import SearchIcon from "./search.svg";
 
 // my api key 191e2350
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=191e2350"
+
+
+const testMovie = {
+  "Poster" : 
+"https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+"Title" : 
+"Batman Begins",
+"Type" : 
+"movie",
+"Year" : 
+"2005",
+"imdbID" : 
+"tt0372784"
+}
+
+
 function App() {
 
   const searchMovies = async (title) => {
@@ -19,8 +36,47 @@ useEffect(() => {
 }, []);
 
   return (
-    <div className="App">
-        <h1>New app </h1> 
+    <div className="app">
+        <h1>Moview hub</h1> 
+        <div className="search">
+            <input 
+            placeholder="search for movies"
+            value="superman"
+            onChange={() => {}}
+            />
+            <img 
+            src={SearchIcon}
+            alt="search"
+            onClick={() => {}}
+            />
+        </div>
+          <div className="container">
+              <div className='movie'>
+                    <div>
+                      <p>{testMovie.Year}</p>
+                    </div>
+
+                    <div>
+                      <img 
+                      src={testMovie.Poster !== "N/A" ? testMovie.Poster : "https://via.placeholder.com/400"}
+                      alt={testMovie.Title}
+                      />
+                    </div>
+
+                    <div>
+                      <span>
+                        {testMovie.Type}
+                      </span>
+                      <h3>
+                        {testMovie.Title}
+                      </h3>
+                    </div>
+              </div>
+          </div>
+
+
+
+
     </div>
   );
 }
