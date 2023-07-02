dadd33db6bcc0f40e1793e393ff33eb05d730c6e
Christian <cedenoc181@gmail.com>
2023-07-02 18:39:53 -0400

    omdb movie data base fetched

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from "react";
 
+// my api key 191e2350
+
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=191e2350"
 function App() {
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+
+      console.log(data.search);
+  }
+
+useEffect(() => {
+    searchMovies('yugioh')
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+New app 
     </div>
   );
 }
